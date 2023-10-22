39f9a7d4902fe46aea58362fd34dd7bd2c191715
Rasmus Löfgren <rasmuslofgren24@gmail.com>
2023-10-22 12:13:03 +0200

    Libra Fixes

--- Assets/Script/Enviroment/LibraScale.cs
+++ Assets/Script/Enviroment/LibraScale.cs
@@ -6,22 +6,25 @@ public class LibraScale : MonoBehaviour
     [SerializeField] private Transform platform1;
     [SerializeField] private Transform ropeMask1;
     [SerializeField] private Vector3 highPos1;
+    [SerializeField] private Vector3 middlePos1;
     [SerializeField] private Vector3 lowPos1;
     [SerializeField] private Transform platform2;
     [SerializeField] private Transform ropeMask2;
     [SerializeField] private Vector3 highPos2;
+    [SerializeField] private Vector3 middlePos2;
     [SerializeField] private Vector3 lowPos2;
 
-    private bool _moving = false;
     private int _dir = 0;
 
     [SerializeField] private float _speed;
 
+    public enum scaleState { one, middle, two, moving}
+    private scaleState _state = scaleState.moving;
 
     // Update is called once per frame
     void Update()
     {
-        if (_moving)
+        if (_state == scaleState.moving)
         {
             if (_dir < 0)
             {
@@ -35,7 +38,7 @@ public class LibraScale : MonoBehaviour
 
                 if (platform1.localPosition == highPos1 && platform2.localPosition == lowPos2)
                 {
-                    _moving = false;
+                    _state = scaleState.one;
                 }
             }
             else if (_dir > 0)
@@ -50,9 +53,25 @@ public class LibraScale : MonoBehaviour
 
                 if (platform1.localPosition == lowPos1 && platform2.localPosition == highPos2)
                 {
-                    _moving = false;
+                    _state = scaleState.middle;
                 }
             }
+            else if (_dir == 0)
+            {
+                platform1.localPosition = Vector2.MoveTowards(platform1.localPosition, middlePos1, Time.deltaTime * _speed);
+                platform2.localPosition = Vector2.MoveTowards(platform2.localPosition, middlePos2, Time.deltaTime * _speed);
+
+                float r = ((platform1.localPosition.y - lowPos1.y) / (highPos1.y - lowPos1.y));
+                ropeMask1.localPosition = new Vector3(0, r, 0);
+                r = ((platform2.localPosition.y - lowPos2.y) / (highPos2.y - lowPos2.y));
+                ropeMask2.localPosition = new Vector3(0, r, 0);
+
+                if (platform1.localPosition == middlePos1 && platform2.localPosition == middlePos2)
+                {
+                    _state = scaleState.two;
+                }
+            }
+
         }
         
 
@@ -60,32 +79,16 @@ public class LibraScale : MonoBehaviour
 
     public void AddObject(int side)
     {        
-        if (_dir == 0)
-        {
-            _moving = true;
-        }
-
         _dir += side;
 
-        if (_dir == 0)
-        {
-            _moving = false;
-        }
+        _state = scaleState.moving;
     }
 
     public void RemoveObject(int side)
     {
-        if (_dir == 0)
-        {
-            _moving = true;
-        }
-
         _dir -= side;
 
-        if (_dir == 0)
-        {
-            _moving = false;
-        }
+        _state = scaleState.moving;
     }
 
 
